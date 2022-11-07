91957e1e68157a745f9808ca4cb743d5f8e0a251
Adam <wilkanowski.adam@gmail.com>
2022-11-07 14:25:45 +0100

    Initial Commit

---
+++ BrainTrainingGame/BrainTrainingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainingGameApp.swift
+//  BrainTrainingGame
+//
+//  Created by Adam Wilkanowski on 07/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct BrainTrainingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
